aa5c31bd8d21b08a77a96f97160768b253a3ccf4
gustavobarez <gbarez44@gmail.com>
2025-09-17 10:43:52 -0300

    feat: add updateTask handler logic

---
+++ src/main/java/br/com/gustavobarez/entity/UpdateTaskRequestDTO.java
@@ -0,0 +1,5 @@
+package br.com.gustavobarez.entity;
+
+public record UpdateTaskRequestDTO(String title, String description, Status status) {
+    
+}
--- src/main/java/br/com/gustavobarez/resource/TaskResource.java
+++ src/main/java/br/com/gustavobarez/resource/TaskResource.java
@@ -3,13 +3,16 @@ package br.com.gustavobarez.resource;
 import java.time.LocalDateTime;
 
 import br.com.gustavobarez.entity.Task;
+import br.com.gustavobarez.entity.UpdateTaskRequestDTO;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -23,6 +26,27 @@ public class TaskResource {
     @Transactional
     public Response createTask(@Valid Task task) {
         task.setCreatedAt(LocalDateTime.now());
+        task.persist();
+        return Response.ok(task).build();
+    }
+
+    @PATCH
+    @Transactional
+    public Response updateTask(@QueryParam("id") Long id, UpdateTaskRequestDTO request) {
+        if (request.title() == null && request.description() == null && request.status() == null) {
+            throw new IllegalArgumentException("All fields cannot be empty");
+        }
+        Task task = Task.findById(id);
+        if (request.title() != null) {
+            task.setTitle(request.title());
+        }
+        if (request.description() != null) {
+            task.setDescription(request.description());
+        }
+        if (request.status() != null) {
+            task.setStatus(request.status());
+        }
+        task.setUpdatedAt(LocalDateTime.now());
         task.persist();
         return Response.ok(task).build();
     }
